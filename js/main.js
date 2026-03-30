e69f3c57870fa7dfc780e3aa631d89a248c64e94
Valentine Sidi <valentinesidi1@gmail.com>
2026-03-30 22:09:04 +0300

    Add main.js as the app entry point for MazaoFlow application

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// js/main.js
+
+// Initialize controllers
+function initControllers() {
+    // Initialize all controllers here
+}
+
+// Set up event delegation
+function setupEventDelegation() {
+    // Event delegation code here
+}
+
+// Application start point
+function startApp() {
+    initControllers();
+    setupEventDelegation();
+}
+
+// Start the application
+startApp();
