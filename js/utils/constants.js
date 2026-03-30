196e9a9f6239f5fccaafbbcd4a12e801e0ff8466
Valentine Sidi <valentinesidi1@gmail.com>
2026-03-30 21:59:06 +0300

    Create js/utils/constants.js file with LISTINGS array, ROLE_INFO object, and FP_SCREENS constant

---
+++ js/utils/constants.js
@@ -0,0 +1,9 @@
+export const LISTINGS = []; // Add listings here
+
+export const ROLE_INFO = {
+    admin: 'Admin',
+    user: 'User',
+    guest: 'Guest'
+};
+
+export const FP_SCREENS = ['login', 'signup', 'forgot-password']; // Add other screens as needed
